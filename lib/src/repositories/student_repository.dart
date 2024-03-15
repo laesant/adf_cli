a1c22c34741b6f205950d83ca8004d0b78e1871e
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-03-15 18:40:41 -0300

    Implementing POST, PUT and DELETE.

--- lib/src/repositories/student_repository.dart
+++ lib/src/repositories/student_repository.dart
@@ -32,9 +32,36 @@ class StudentRepository {
     return Student.fromJson(response.body);
   }
 
-  Future<void> insert(Student student) async {}
+  Future<void> insert(Student student) async {
+    final response = await http.post(
+      Uri.parse('http://localhost:8080/students'),
+      body: student.toJson(),
+      headers: {'content-type': 'application/json'},
+    );
 
-  Future<void> update(Student student) async {}
+    if (response.statusCode != 200) {
+      throw Exception();
+    }
+  }
 
-  Future<void> deleteById(int id) async {}
+  Future<void> update(Student student) async {
+    final response = await http.put(
+      Uri.parse('http://localhost:8080/students/${student.id}'),
+      body: student.toJson(),
+      headers: {'content-type': 'application/json'},
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception();
+    }
+  }
+
+  Future<void> deleteById(int id) async {
+    final response =
+        await http.delete(Uri.parse('http://localhost:8080/students/$id'));
+
+    if (response.statusCode != 200) {
+      throw Exception();
+    }
+  }
 }
