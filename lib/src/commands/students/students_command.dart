f80a648f3e6c90fe24ed9ab80e2c81ad2e7c9c9a
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-03-16 13:46:12 -0300

    Insert new students

--- lib/src/commands/students/students_command.dart
+++ lib/src/commands/students/students_command.dart
@@ -3,6 +3,7 @@ import 'package:args/command_runner.dart';
 import '../../repositories/student_repository.dart';
 import 'subcommands/find_all_command.dart';
 import 'subcommands/find_by_id_command.dart';
+import 'subcommands/insert_command.dart';
 
 class StudentsCommand extends Command {
   @override
@@ -15,5 +16,6 @@ class StudentsCommand extends Command {
     final repository = StudentRepository();
     addSubcommand(FindAllCommand(repository));
     addSubcommand(FindByIdCommand(repository));
+    addSubcommand(InsertCommand(repository));
   }
 }
---
+++ lib/src/commands/students/subcommands/insert_command.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:args/command_runner.dart';
+
+import '../../../models/address.dart';
+import '../../../models/city.dart';
+import '../../../models/phone.dart';
+import '../../../models/student.dart';
+import '../../../repositories/product_repository.dart';
+import '../../../repositories/student_repository.dart';
+
+class InsertCommand extends Command {
+  final StudentRepository studentRepository;
+  final ProductRepository productRepository;
+
+  InsertCommand(this.studentRepository)
+      : productRepository = ProductRepository() {
+    argParser.addOption('file', help: 'Path of the csv file', abbr: 'f');
+  }
+
+  @override
+  String get description => 'Insert Student';
+
+  @override
+  String get name => 'insert';
+
+  @override
+  FutureOr? run() async {
+    print('Aguarde...');
+    final filePath = argResults?['file'];
+    final students = File(filePath).readAsLinesSync();
+    print('-------------------------------------------');
+    for (var student in students) {
+      final data = student.split(';');
+      final courseCsv = data[2].split(',').map((e) => e.trim()).toList();
+      final coursesFutures = courseCsv.map((c) async {
+        final course = await productRepository.findByName(c);
+        course.isStudent = true;
+        return course;
+      }).toList();
+      final courses = await Future.wait(coursesFutures);
+      final studentModel = Student(
+        name: data[0],
+        age: int.tryParse(data[1]),
+        nameCourses: courseCsv,
+        courses: courses,
+        address: Address(
+          street: data[3],
+          number: int.parse(data[4]),
+          zipCode: data[5],
+          city: City(id: 1, name: data[6]),
+          phone: Phone(ddd: int.parse(data[7]), phone: data[8]),
+        ),
+      );
+
+      await studentRepository.insert(studentModel);
+    }
+    print('Alunos adicionados com sucesso!');
+    print('-------------------------------------------');
+  }
+}
